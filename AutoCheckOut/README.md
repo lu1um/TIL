f56cc5c35609ff67b47f8d7b023839694f8076ec
lu1um <emperor1812@gmail.com>
2022-01-19 17:58:29 +0900

    01-19

--- AutoCheckOut/README.md
+++ AutoCheckOut/README.md
@@ -1,11 +1,14 @@
 # make Auto checkout by python
 
-** 참고 문헌 : https://sir.kr/so_python/273 **
+** 참고 문헌 **
+1. https://sir.kr/so_python/273
+2. https://pythondocs.net/selenium/셀레니움-wait-개념-이해하기-implicitly-wait-vs-explicitly-wait/
 
 1. chrome 설치
 2. selenium 설치
 3. chromedriver 설치 - 같은 디렉토리에 넣기!
 
 문제점 : Path.cwd()는 현재 터미널의 작업공간 주소를 불러온다..
+ -> __file__ 객체를 사용함으로 해결
 
-같은 폴더내의 URL.txt를 만들고, 클릭하기 원하는 URL, XPath를 저장해놓았다.
+같은 폴더내의 URL.txt, PASSWORD.txt를 만들고, 클릭하기 원하는 URL, XPath, ID, 비번을 저장해놓았다.
---
+++ AutoCheckOut/login.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from time import sleep
+
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import  ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+MAIN_PATH = __file__ + '\\..\\'
+
+class AutoCheckOut:
+    def __init__(self, urltxt, idtxt):
+        self.__url = str()
+        self.__xPath = str()
+        self.__id = str()
+        self.__password = str()
+        self.driver = Chrome(MAIN_PATH + 'chromedriver.exe')
+
+        self.loadURL(urltxt)
+        self.loadID(idtxt)
+
+    def loadURL(self, urltxt):
+        with open(MAIN_PATH + urltxt, 'r') as F:
+            self.__url = F.readline()
+            self.__url = self.__url.strip('\n')
+            self.__xPath = F.readline()
+
+    def loadID(self, idtxt):
+        with open(MAIN_PATH + idtxt, 'r') as F:
+            self.__id = F.readline()
+            self.__id = self.__id.strip('\n')
+            self.__password = F.readline()
+
+    def openURL(self, waitForElement):
+        self.driver.get(self.__url)
+        self.driver.implicitly_wait(3)
+        # element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, waitForElement)))
+
+    def login(self):
+        _id = self.driver.find_element_by_xpath('//*[@id="userId"]')
+        _password = self.driver.find_element_by_xpath('//*[@id="userPwd"]')
+
+        _id.send_keys(self.__id)
+        _password.send_keys(self.__password)
+
+        sleep(1)
+
+        self.driver.find_element_by_xpath('//*[@id="wrap"]/div/div/div[2]/form/div/div[2]/div[3]/a').click()
+
+    def thisURL(self): # 지금 이 페이지가 뭔지 알려주는 함수
+        pass
+
+    def xPath(self):
+        return self.__xPath
+
+    def test(self):
+        print(self.__url)
+        print(self.CHECKOUT_XPATH)
