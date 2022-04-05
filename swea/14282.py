aa03e5530ee3462c63eb13a7a70b3b1d82f9eac1
lu1um <emperor1812@gmail.com>
2022-04-05 16:55:48 +0900

    0405

--- swea/14282.py
+++ swea/14282.py
@@ -1,33 +1,30 @@
 import sys
 sys.stdin = open('input.txt')
-# failed
+
+from collections import deque
 
 DELTA = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-
-def dfs(i, j, fuel, visited):
-    global min_fuel
-    if fuel > min_fuel:
-        return
-    if i == N-1 and j == N-1:
-        min_fuel = fuel
-        return
-    for d in DELTA:
-        di = i + d[0]
-        dj = j + d[1]
-        if 0<=di<N and 0<=dj<N and (di, dj) not in visited:
-            if arr[di][dj] > arr[i][j]:
-                _fuel = fuel + arr[di][dj]-arr[i][j] + 1
-            else:
-                _fuel = fuel + 1
-            dfs(di, dj, _fuel, visited+[(di, dj)])
-
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
+    dp = [[float('inf')]*N for _ in range(N)]
+    dp[0][0] = 0
 
-    min_fuel = float('inf')
-    dfs(0, 0, 0, [])
-    print(f'#{tc} {min_fuel}')
+    queue = deque([(0, 0)])
+    while queue:
+        i, j = queue.popleft()
+        for d in DELTA:
+            di = i + d[0]
+            dj = j + d[1]
+            if 0<=di<N and 0<=dj<N:
+                height = 1
+                if arr[di][dj] > arr[i][j]:
+                    height += arr[di][dj] - arr[i][j]
+                fuel = dp[i][j] + height
+                if fuel < dp[di][dj]:
+                    dp[di][dj] = fuel
+                    queue.append((di, dj))
+    print(f'#{tc} {dp[N-1][N-1]}')
 
